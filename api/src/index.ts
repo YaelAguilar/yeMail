a09aa06ff391a7cda281dad24b7177a2ac7caa3a
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-01-21 23:13:06 -0600

    [UPDATE] DB conection

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,23 +1,29 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import userRoutes from '../src/interfaces/rest/UserRoutes'
 import emailRoutes from '../src/interfaces/rest/EmailRoutes'
 import { errorHandler } from '../src/middleware/errorHandler';
 import { startWebSocketServer } from './infrastructure/websocket/WebSocketServer';
 import { startSocketIOServer } from './infrastructure/socketio/SocketIOServer';
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Conexión a MongoDB
-mongoose.connect('la baseeeeeee')
+const dbUri = process.env.DB_URI;
+if(!dbUri){
+    console.error('La variable DB_URI no está definida en .env');
+    process.exit(1);
+}
+
+mongoose.connect(dbUri)
     .then(() => console.log('Conectado a la DB'))
     .catch(err => console.error('Error al conectar a la DB', err));
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/emails', emailRoutes);
-
 // Middleware de manejo de errores
 app.use(errorHandler);
 
