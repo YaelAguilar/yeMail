21a4a51abff34ce466af25f9802bc62a5d0f6d3b
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-01-21 18:50:01 -0600

    [ADD] instances on router

--- api/src/interfaces/Route/UserRoutes.ts
+++ api/src/interfaces/Route/UserRoutes.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import { UserController } from '../controller/UserController';
+import { UserService } from '../../application/service/UserService';
+import { UserRepository } from '../../domain/repository/UserRepository';
 
 const router = express.Router();
-const userController = new UserController();
+
+const userRepository = new UserRepository();
+const userService = new UserService(userRepository);
+const userController = new UserController(userService);
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
