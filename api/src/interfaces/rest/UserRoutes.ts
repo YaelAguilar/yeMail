febbc57f301d87c83730db5bf520913454198fe5
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-01-21 18:55:40 -0600

    [EDIT] folder treee, create folder rest

--- api/src/interfaces/rest/UserRoutes.ts
+++ api/src/interfaces/rest/UserRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { UserController } from '../controller/UserController';
+import { UserController } from './UserController';
 import { UserService } from '../../application/service/UserService';
 import { UserRepository } from '../../domain/repository/UserRepository';
 
