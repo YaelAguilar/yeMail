a5ef7704fd00e84b7cd3da288a4b6be7ebb786d9
YaelAguilar <213452@ids.upchiapas.edu.mx>
2024-01-21 21:42:24 -0600

    [ADD] index

---
+++ api/src/index.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import userRoutes from '../src/interfaces/rest/UserRoutes'
+import emailRoutes from '../src/interfaces/rest/EmailRoutes'
+import { errorHandler } from '../src/middleware/errorHandler';
+import { startWebSocketServer } from './infrastructure/websocket/WebSocketServer';
+import { startSocketIOServer } from './infrastructure/socketio/SocketIOServer';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Conexión a MongoDB
+mongoose.connect('tu-cadena-de-conexión-a-MongoDB')
+    .then(() => console.log('Conectado a la DB'))
+    .catch(err => console.error('Error al conectar a la DB', err));
+
+app.use(express.json());
+app.use('/api/users', userRoutes);
+app.use('/api/emails', emailRoutes);
+
+// Middleware de manejo de errores
+app.use(errorHandler);
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+// Iniciar servidores WebSocket y Socket.io
+startWebSocketServer(server);
+startSocketIOServer(server);
